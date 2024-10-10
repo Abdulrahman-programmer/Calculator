9104ba795898f6dee60f52c89d05a80fea621788
Abdulrahman-programmer <152911698+Abdulrahman-programmer@users.noreply.github.com>
2024-10-10 21:56:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,69 @@
 let display = document.getElementById("display")
-let btn = document.getElementsByClassName("input")
-btn.addEventListener("click",
-    function()
-    {
-        if(display.textContent === "ERROR")
-        {
-            display.textContent = "" 
-        }
-    }
-)
+
 function one() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     display.textContent += "1"
 }
 function two() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     display.textContent += "2"
 }
 function three() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     display.textContent += "3"
 }
 function four() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     display.textContent += "4"
 }
 function five() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     display.textContent += "5"
 }
 function six() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     display.textContent += "6"
 }
 function seven() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     display.textContent += "7"
 }
 function eight() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     display.textContent += "8"
 }
 function nine() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     display.textContent += "9"
 }
 function zero() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     display.textContent += "0"
 }
 function dot() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     if (!display.textContent.includes(".")) {
         display.textContent += ".";
     }
@@ -48,35 +72,56 @@ function allClear() {
     display.textContent = ""
 }
 function plus() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     if (display.textContent) {
         display.textContent += "+"
     }
 }
 function minus() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     if (display.textContent) {
         display.textContent += "-"
     }
 }
 function multiply() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     if (display.textContent) {
         display.textContent += "×"
     }
 }
 function division() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     if (display.textContent) {
         display.textContent += "/"
     }
 }
 function percent() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     if (display.textContent) {
         display.textContent += "%"
     }
 }
 function dlt() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     display.textContent = (display.textContent).slice(0, -1)
 }
 
 function result() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     try {
         let sum = display.textContent.replace("×", "*")
         display.textContent = eval(sum)
@@ -91,6 +136,9 @@ function result() {
 let openParenthesis = true
 
 function addParenthesis() {
+    if (display.textContent === "ERROR") {
+        display.textContent = ""
+    }
     if (openParenthesis) {
         display.textContent += "(";
         openParenthesis = false;
